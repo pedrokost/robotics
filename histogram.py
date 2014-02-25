c33ad74a4df4db4666f7a535e7ec8345a3c7887a
Pedro Kostelec <pedrokost@gmail.com>
2014-02-25 11:06:01 +0000

    Histogram stuff

---
+++ histogram.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Histogram:
+	"""
+	Represents a histogram of datapoint
+	"""
+	def __init__(self, vector, nBins = 20):
+		"""
+		The vector contains reliable scalar values (in case of distance measurments, only those where the distance is below 100cm, and angle to wall < 40 deg)
+		"""
+		self.values = np.array(vector)
+		self.hist = self.hist(vector, nBins)
+
+	def hist(self, vector, nBins):
+		h, _ = np.histogram(vector, nBins)
+		return h
